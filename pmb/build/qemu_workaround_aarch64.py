650046bf9751958a115e2a6b093cfbcbdb5c1052
Oliver Smith <ollieparanoid@bitmessage.ch>
2017-11-14 22:12:03 +0100

    Clean up

--- pmb/build/qemu_workaround_aarch64.py
+++ pmb/build/qemu_workaround_aarch64.py
@@ -16,26 +16,18 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with pmbootstrap.  If not, see <http://www.gnu.org/licenses/>.
 """
-import pmb.chroot
-import pmb.chroot.apk_static
-import pmb.config
-import pmb.helpers.repo
-import pmb.helpers.run
-import pmb.parse.arch
+import pmb.build.package
+import pmb.chroot.apk
 
 
 def qemu_workaround_aarch64(args, suffix="buildroot_aarch64"):
     """
     Qemu has a bug in aarch64 emulation, that causes abuild-tar to omit files
-    from the archives it generates in some cases. This workaround copies the
-    abuild-tar binary from the native chroot into the buildroot_aarch64 and
-    installs a wrapper to use it. That way we bypass emulation and it works as
-    expected again.
+    from the archives it generates in some cases. We build a patched abuild-tar,
+    which avoids the bug.
 
     https://github.com/postmarketOS/pmbootstrap/issues/546
     """
-
-    # Build and install abuild-aarch64-qemu-workaround
     pkgname = "abuild-aarch64-qemu-workaround"
     pmb.build.package(args, pkgname, "aarch64", True,
                       init_buildenv=False)
